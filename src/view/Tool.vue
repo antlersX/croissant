408deae0113efc3f1937d23b2fcf193b27647222
jzx <945627077@qq.com>
2022-03-30 16:45:01 +0800

    '修改显示颜色'

--- src/view/Tool.vue
+++ src/view/Tool.vue
@@ -1,39 +1,60 @@
 <template>
-    <div class="mx-1 flex items-center justify-center" >
-        <div class=" border rounded-full px-5  my-1 mx-1 cursor-pointer">upload</div>
-        <div class="  border rounded-full text-xs p-1 px-2 bg-green-500 text-white" v-if="device!=null">{{device?.productName+":"+device?.productId }} </div>
-        <div class=" border rounded-full px-5  my-1 mx-1 cursor-pointer"
-        :class="{
-            'bg-red-500 text-white':device==null
-        }"
-         @click="selectDevice">select</div>
+  <div class="mx-1 flex items-center justify-center">
+    <div
+      class="border rounded-full px-5 my-1 mx-1 cursor-pointer"
+      :class="{
+        'bg-red-500 text-white': device == null,
+      }"
+      @click="selectDevice"
+    >
+      select device
     </div>
+    <div
+      class="border rounded-full text-xs p-1 px-2 bg-green-500 text-white"
+      v-if="device != null"
+    >
+      {{ device?.productName + ":" + device?.productId }}
+    </div>
+    <div
+      v-if="device != null"
+      class="
+        border
+        rounded-full
+        px-5
+        my-1
+        mx-1
+        cursor-pointer
+        hover:bg-gray-200 
+      "
+    >
+      upload
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name:"Tool",
-  data(){
-      return {
-          device:null
-      }
+  name: "Tool",
+  data() {
+    return {
+      device: null,
+    };
   },
-  methods:{
-      selectDevice(){
-              navigator.usb.getDevices()
-                .then(devices => {
-                navigator.usb.requestDevice({filters: []})
-                .then(usbDevice => {
-                        this.device=usbDevice
-                        console.log(this.device);
-                })
-                .catch(e => {
-                console.log("There is no device. " + e);
-                });
-                })
-      }
+  methods: {
+    selectDevice() {
+      navigator.usb.getDevices().then((devices) => {
+        navigator.usb
+          .requestDevice({ filters: [] })
+          .then((usbDevice) => {
+            this.device = usbDevice;
+            console.log(this.device);
+          })
+          .catch((e) => {
+            console.log("There is no device. " + e);
+          });
+      });
+    },
   },
-  mounted(){
-  }
-}
+  mounted() {},
+};
 </script>
